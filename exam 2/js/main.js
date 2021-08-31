eed3df238d7af05f25bec2cc32dd2c9de356b91b
Ali Xudiyev <87548328+khudieff@users.noreply.github.com>
2021-08-31 13:57:26 +0400

    Add files via upload

---
+++ exam 2/js/main.js
@@ -0,0 +1,38 @@
+var a=5
+console.log(a*1000 +"m");
+
+
+
+
+var arr=[10,68,987,7,1,8]
+function findmax(x,y) {
+for (let i = 0; i < arr.length; i++) {
+if (x,y){
+    return x-y;
+}
+    
+}
+}
+console.log(arr);
+
+
+
+
+
+
+
+
+
+var ad="Anar"
+var array=["Anar", "Hikmet", "Seymur"]
+function namefind(array){
+    for (let i = 0; i < array.length; i++) {
+      if (ad=array[i]){
+          return true
+      }
+        else {
+            return false
+        }
+    }
+}
+console.log(namefind(array));
